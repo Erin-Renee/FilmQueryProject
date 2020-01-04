dd9933dd75e2637d4442d94f2e6a76cb9024b08b
Erin Eustice <erinrenee.va@gmail.com>
2020-01-04 16:56:10 -0700

    user story number 1 complete, application is able to look up film by id

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -8,7 +8,6 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-	Scanner kb = new Scanner(System.in);
   
   DatabaseAccessor db = new DatabaseAccessorObject();
   
@@ -16,8 +15,8 @@ public class FilmQueryApp {
   public static void main(String[] args) throws SQLException{
 	  
     FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
+//    app.test();
+  app.launch();
   }
 
   private void test() throws SQLException {
@@ -29,53 +28,60 @@ public class FilmQueryApp {
     Scanner input = new Scanner(System.in);
     startUserInterface(input);
     
-//    try {
-//		System.out.println("Film Query Menu");
-//		int choice;
-//		boolean menureturn = true;
-//		while (menureturn) {
-//			menu();
-//			choice = input.nextInt();
-//
-//			switch (choice) {
-//			case 1:
-//				
-//				break;
-//			case 2:
-//				
-//				break;
-//			case 3:
-//				System.out.println(" Exiting.... Goodbye. ");
-//				menureturn = false;
-//				break;
-//			default:
-//				System.out.println("Please select a choice from menu (1-3)");
-//				break;
-//
-//			}// switch
-//		} // while
-//	} // try
-//	catch (Exception e) {
-//		System.out.println("ERROR RESTART PROGRAM");
-//
-//	}
-//
-//}
-    
+    try {
+		System.out.println("Film Query Menu");
+		int choice;
+		boolean menureturn = true;
+		while (menureturn) {
+			menu();
+			choice = input.nextInt();
+
+			switch (choice) {
+			case 1:
+				filmById(input);
+				break;
+			case 2:
+				
+				break;
+			case 3:
+				System.out.println(" Exiting.... Goodbye. ");
+				menureturn = false;
+				break;
+			default:
+				System.out.println("Please select a choice from menu (1-3)");
+				break;
+
+			}// switch
+		} // while
+	} // try
+	catch (Exception e) {
+		System.out.println("ERROR RESTART PROGRAM");
+
+	}
+
   input.close();
-  }
+}
+    
+  
 
   private void startUserInterface(Scanner input) {
     
   }
   
-//  public void menu() {
-//		System.out.println("----------------------------------------");
-//		System.out.println("1: Look up film by ID ");
-//		System.out.println("2: Look up a film by a search keyword ");
-//		System.out.println("3: Exit ");
-//		System.out.println("----------------------------------------");
-//
-//	}
+  public void menu() {
+		System.out.println("----------------------------------------");
+		System.out.println("1: Look up film by ID ");
+		System.out.println("2: Look up a film by a search keyword ");
+		System.out.println("3: Exit ");
+		System.out.println("----------------------------------------");
 
+	}
+  
+  public void filmById(Scanner input) {
+	  int idfilm;
+	  System.out.println("Please enter film ID: ");
+	  idfilm = input.nextInt();
+	  Film film = db.findFilmById(idfilm);
+	  System.out.println(film);
+  }
 }
